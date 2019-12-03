07263ea7ff63fbcf41a81db708b94561e03170ed
Samik Gupta <samik.gupta@broadcom.com>
2019-12-03 13:12:17 -0800

    add support for the Broadcom iProc SoC Family
    Enable support for systems based on Broadcom iProc architected SoCs. The
    iProc complex contains one or more ARM CPUs along with common core
    peripherals. Application specific SoCs are created by adding a
    uArchitecture containing peripherals outside of the iProc complex.
    
    Add a sub-target for the PS225 adapter, based on the BCM5880X ASIC from
    the iProc family.
    
    PS225 is a PCI Express low-profile, half-length adapter and provides
    two 25-Gigabit Ethernet (25GbE) ports along with eight 64-bit ARMv8
    processors running at 3.0 GHz. On-board DDR4 memory sizes range from 4GB
    to 16GB. It is designed to run high-performance networking, storage and
    management workloads.
    
    Signed-off-by: Samik Gupta <samik.gupta@broadcom.com>

---
+++ target/linux/brcmiproc/bcm58800/target.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2018 Jianhui Zhao <jianhuizhao329@gmail.com>
+# Copyright (C) 2019 Samik Gupta <samik.gupta@broadcom.com>
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+SUBTARGET:=bcm58800
+BOARDNAME:=BCM58800 based boards
+KERNELNAME:=Image
+
+define Target/Description
+	Build image for boards based on Broadcom iProc/brcm58800 SoC
+endef
